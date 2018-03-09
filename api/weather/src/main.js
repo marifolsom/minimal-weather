9cb691f12684299c9911bbb75ed000b87e05ae88
marifolsom <mfolsom1@fordham.edu>
2018-03-09 17:39:33 -0500

    set up getZipcode function and started getWeather

--- api/weather/src/main.js
+++ api/weather/src/main.js
@@ -17,3 +17,43 @@ STEPS
   - append the data to the DOM
 
 */
+
+
+// Make a variable to hold the zipcodeInput value, default: Anchorage
+let zipcode = 99501;
+
+
+// Make a function that creates an API request and returns a response in json
+let getJSON = (url) => {
+  return fetch(url).then(response => response.json());
+}
+
+
+// Make a function that creates an API request with the user's input
+let getWeather = (zipcode) => {
+  // Make an API request with the user's inputted zipcode
+  getJSON(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&APPID=cfe78bea462ec765e53c4ee7dc76d7ec`)
+  .then(json => {
+    console.log(json);
+    // City name, current temperature, weather description, min temp, max temp
+    // Keep track of what you're dealing with... object? array?
+    // Math.floor for kelvin conversion!
+  });
+}
+getWeather(zipcode);
+
+
+// Make a function for that grabs the user's input and stores the value
+let getZipCode = (evt) => {
+  // Prevent reload on submit default
+  evt.preventDefault();
+  // Update the zipcode variable with the user's input
+  let zipcodeInput = document.querySelector('.input');
+  zipcode = zipcodeInput.value;
+  console.log(zipcode);
+}
+
+
+// When the page loads, add an event listener to the submit button
+let submitButton = document.querySelector('.submit');
+submitButton.addEventListener('click', getZipCode);
